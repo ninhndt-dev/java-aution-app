9cbf4edfaff65b98e9a10c813ae50d670e41e9c0
ninhndt <ninhndt@rabiloo.com>
2026-06-22 22:59:28 +0700

    add Hello.java practice file

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Java version đang chạy: " + System.getProperty("java.version"));
+        System.out.println("Tên JVM: " + System.getProperty("java.vm.name"));
+    }
+}
